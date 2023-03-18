2d74642e68b7e5939d3a6f23c799b132e160c89c
oracle123 <lington12@gmail.com>
2023-03-18 08:48:35 -0700

    0x12 Javascriot introduction

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+const len = process.argv.length;
+argCount <= 2 && console.log('No argument');
+argCount === 3 && console.log('Argument found');
+argCount > 3 && console.log('Arguments found');
